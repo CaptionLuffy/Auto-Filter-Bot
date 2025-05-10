8e126eeef7b8219c87cc660a3921c17aa36df63a
CaptionLuffy <159707662+CaptionLuffy@users.noreply.github.com>
2025-05-10 21:10:14 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -8,7 +8,14 @@ media_filter = filters.document | filters.video
 @Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
 async def media(bot, message):
     """Media Handler"""
-    media = getattr(message, message.media.value, None)
+    '''media = getattr(message, message.media.value, None)
     if (str(media.file_name).lower()).endswith(tuple(INDEX_EXTENSIONS)):
         media.caption = message.caption
+        await save_file(media)'''
+    
+    media = getattr(message, message.media.value, None)
+    if media.mime_type in ['video/mp4', 'video/x-matroska']:  # Non .mp4 and .mkv files are skipped
+        media.file_type = message.media.value
+        media.caption = message.caption
         await save_file(media)
+
